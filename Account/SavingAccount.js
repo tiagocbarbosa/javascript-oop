2b436d94708f3fe08ae454743564d08926724a43
Tiago Barbosa <tiagocastrobarbosa@gmail.com>
2020-09-21 19:43:13 -0300

    SavingAccount class finished

---
+++ Account/SavingAccount.js
@@ -0,0 +1,10 @@
+import { Account } from './Account.js'
+
+export class SavingAccount extends Account {
+
+    // *** Methods ***
+
+    constructor(initialBalance, client, agency) {
+        super(initialBalance, client, agency)
+    }
+}
