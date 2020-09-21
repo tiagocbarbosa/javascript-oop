69d0bafc5871574dd726b642324a3d647bc3cd9c
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2020-09-21 11:51:28 -0300

    Account class finished

--- Account/Account.js
+++ Account/Account.js
@@ -1,9 +1,61 @@
+import { Client } from '../Client.js'
+
 export class Account {
+
+    // *** Accessors ***
+
+    get client() {
+        return this.client
+    }
+    
+    set client(newClient) {
+        if(newClient instanceof Client) {
+            this._client = newClient
+        }
+    }
+
+    get balance() {
+        return this._balance
+    }
+    
+    // *** Methods ***
+
     constructor(initialBalance, client, agency) {
+        if(this.constructor == Account) {
+            throw new Error('You cannot initialize an Abstract class of the type Account')
+        }
+
         this._balance = initialBalance
-        // here it's a proposal to a private property
-        //this.#balance = initialBalance (https://github.com/tc39/proposal-class-fields#private-fields)
         this.client = client
         this.agency = agency
+
+        // in the code below you can check a proposal for private fields using '#'
+        // this.#balance = initialBalance (https://github.com/tc39/proposal-class-fields#private-fields)
+    }
+    
+    // Abstract method
+    withdraw(value) {
+        // I'm launching an error if this method is not overwritten
+        throw new Error('Abstract methods must be overwritten')
+    }
+
+    _withdraw(value, tax) {
+        const amountDrawn = value * tax
+        if(this._balance >= amountDrawn) {
+            this._balance -= amountDrawn
+            return amountDrawn
+        }
+
+        return 0
+    }
+
+    deposit(value) {
+        if(value <= 0) { return }
+        this._balance += value
+    }
+
+    transfer(value, account) {
+        const amountDrawn = this.withdraw(value)
+        account.deposit(amountDrawn)
     }
 }
--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 import { Client } from './Client.js'
+import { Account } from './Account/Account.js'
 
-// *** Printing a Client info ***
-
+// *** Printing a Client example ***
 const clientName = 'Tiago'
 const clientCPF = '710.229.300-36'
 const clientPassword = 1234
-
 const client = new Client(clientName, clientCPF, clientPassword)
 console.log(client)
 
 // client has only a cpf getter, so because of this you cannot set a property cpf
 const cpf = client.cpf // ok
 //client.cpf = 'Lucas' // not ok
+
+// *** Printing Accounts examples ***
