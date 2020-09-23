9925678b763f1f8930659c6ca2d2dd78f27b24cb
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2020-09-23 17:09:00 -0300

    SalaryAccount class created

---
+++ Account/SalaryAccount.js
@@ -0,0 +1,15 @@
+import { Account } from './Account.js'
+
+export class SalaryAccount extends Account {
+
+    // *** Methods ***
+
+    constructor(client) {
+        super(0, client, 100)
+    }
+
+    withdraw(value) {
+        const tax = value * 1.01
+        return this._withdraw(value, tax)
+    }
+}
