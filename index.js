0d5002c057c39fa33e104000d49c5d7153869908
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2020-09-23 17:50:08 -0300

    Employee logic added

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 import { Client } from './Client.js'
-import { Account } from './Account/Account.js'
 import { CheckingAccount } from './Account/CheckingAccount.js'
+import { Employee } from './Employee/Employee.js'
 
+/* *********************** */
 /* Printing Client example */
+/* *********************** */
 
 const clientName = 'Tiago'
 const clientCPF = '710.229.300-36'
 const clientPassword = 1234
 const client = new Client(clientName, clientCPF, clientPassword)
-console.log(client)
+console.log(`A Client object: ${JSON.stringify(client)} \n`)
 
-// client has only a cpf getter, so because of this you cannot set a property cpf
+// client has only a cpf getter, so you cannot set a property cpf
 const cpf = client.cpf // ok
 //client.cpf = 'Lucas' // not ok
 
+/* ************************** */
 /* Printing Accounts examples */
+/* ************************** */
 
 // printing the static property from CheckingAccount class
 const checkingAccount = new CheckingAccount(client, 123)
-console.log(CheckingAccount.accountsNumber)
+console.log(`CheckingAccount objects quantity: ${CheckingAccount.accountsNumber} \n`)
+
+/* *************************** */
+/* Printing Employees examples */
+/* *************************** */
+
+const employeeName = 'Lucas'
+const employeeSalary = 4000
+const employeeCPF = '755.339.980-98'
+const employee = new Employee(employeeName, employeeSalary, employeeCPF)
+employee.registerPassword(5678)
+console.log(`An Employee object: ${JSON.stringify(employee)} \n`)
+
+// passing a wrong password
+const wrongPassword = 9568
+console.log(`Is he authenticated? ${employee.authenticate(wrongPassword) ? 'Yes' : 'No'}`) // No
