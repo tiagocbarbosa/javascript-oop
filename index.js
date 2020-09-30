5c7b6538e9b0c1b95f012c07b9f1924fea8fc142
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2020-09-30 15:22:51 -0300

    AuthenticationSystem logic added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import { Client } from './Client.js'
 import { CheckingAccount } from './Account/CheckingAccount.js'
 import { Employee } from './Employee/Employee.js'
 import { Manager } from './Employee/Manager.js'
+import { AuthenticationSystem } from './AuthenticationSystem.js'
 
 // A Client example
 const clientName = 'Tiago'
@@ -10,7 +11,7 @@ const clientPassword = 1234
 const client = new Client(clientName, clientCPF, clientPassword)
 console.log(`A Client object: ${JSON.stringify(client)} \n`)
 
-// Client has only a cpf getter, so you cannot set a property cpf
+// client has only a cpf getter, so you cannot set a property cpf
 const cpf = client.cpf // ok
 //client.cpf = 'Lucas' // not ok
 
@@ -26,13 +27,21 @@ const employee = new Employee(employeeName, employeeSalary, employeeCPF)
 employee.registerPassword(5678)
 console.log(`An Employee object: ${JSON.stringify(employee)} \n`)
 
-// Passing a wrong password
+// passing a wrong password
 const wrongPassword = 9568
 console.log(`Is he authenticated? ${employee.authenticate(wrongPassword) ? 'Yes' : 'No'} \n`) // No
 
+// A Manager example
 const managerName = 'Daniel'
 const managerSalary = 5000
 const managerCPF = '509.297.050-25'
 const manager = new Manager(managerName, managerSalary, managerCPF)
 manager.registerPassword(9123)
 console.log(`A Manager object: ${JSON.stringify(manager)} \n`)
+
+// A Client object with correct password but without 'authenticate' function
+console.log(AuthenticationSystem.login(client, clientPassword)) // false
+// An Employee object with wrong password but with 'authenticate' function
+console.log(AuthenticationSystem.login(employee, wrongPassword)) // false
+// A Manager object with correct password and 'authenticate' function
+console.log(AuthenticationSystem.login(manager, 9123)) // true
