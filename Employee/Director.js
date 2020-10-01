5f8b3d2e9ce58fdfa6e07b7169abcbf95131b4b7
Tiago Castro Barbosa <tiago.c.barbosa@ericssoninovacao.com.br>
2020-10-01 14:11:30 -0300

    Comments added

--- Employee/Director.js
+++ Employee/Director.js
@@ -1,6 +1,9 @@
 import { Employee } from './Employee.js'
 
 export class Director extends Employee {
+
+    // *** Methods ***
+    
     constructor(name, salary, cpf) {
         super(name, salary, cpf)
         this._bonus = 2
--- Employee/Manager.js
+++ Employee/Manager.js
@@ -1,6 +1,9 @@
 import { Employee } from './Employee.js'
 
 export class Manager extends Employee {
+
+    // *** Methods ***
+    
     constructor(name, salary, cpf) {
         super(name, salary, cpf)
         this._bonus = 1.1
